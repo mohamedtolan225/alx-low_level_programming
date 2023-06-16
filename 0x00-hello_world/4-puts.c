b7d99d0e1df72b641cc3154fa91f7a192598d10b
mohamedtolan225 <mohamedtolan225#gmail.com>
2023-06-16 20:26:23 +0300

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
-
-/*
+/**
  * main - Entry point
  *
  * Return : Always 0 (Success)
@@ -9,5 +8,5 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
