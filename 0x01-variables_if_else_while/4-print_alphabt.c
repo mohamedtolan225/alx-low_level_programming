de2e18e7f043140149381e8b4ed4a6c2ef521c27
mohamedtolan225 <mohamedtolan225#gmail.com>
2023-06-17 02:23:56 +0300

    alph without

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include<stdio>
+
+/**
+ *main -  prints the alphabet in lowercase except q and e
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alph[24] = "abcdfghijklmnoprstuvwxyz";
+	int i;
+	for (i = 0; i < 24 ; i++)
+	{
+	putchar(alph[i]);
+	}
+	putchar("\n");
+	return(0);
+}
