2008d72dd9b390b12d7ec97c1b4da8ff14620fca
mohamedtolan225 <mohamedtolan225#gmail.com>
2023-06-17 01:58:18 +0300

    alp

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio>
+/**
+ * main - print the alphabet.
+ *
+ * Retrurn: Always 0 (success)
+ */
+int main(void)
+{
+	char alp[26]="abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i=0;i<26,i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar("\n");
+	return (0);
+}
