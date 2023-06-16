f81f8652c1924c90567fd19cbe2d127f497d1b6c
mohamedtolan225 <mohamedtolan225#gmail.com>
2023-06-17 02:01:10 +0300

    alp

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - print the alphabet.
- *
- * Retrurn: Always 0 (success)
- */
+* main - Prints the alphabet.
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	char alp[26]="abcdefghijklmnopqrstuvwxyz";
-	int i;
-
-	for (i=0;i<26,i++)
-	{
-		putchar(alp[i]);
-	}
-	putchar("\n");
-	return (0);
+char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+for (i = 0; i < 26; i++)
+{
+putchar(alp[i]);
+}
+putchar('\n');
+return (0);
 }
