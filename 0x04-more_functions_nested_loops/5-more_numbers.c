1eb16a55c5419da832f9f9148d6258b86f0a1ed6
mohamedtolan225 <mohamedtolan225#gmail.com>
2023-06-23 02:56:18 +0300

    asdas

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,20 +6,17 @@
 
 void more_numbers(void)
 {
-	int num, row, count;
+	int c;
+	int t;
 
-	for (row = 1; row <= 10; row++)
+	for (t = 0; t < 10; t++)
 	{
-		for (count = 0; count <= 14; count++)
+		for (c = 0; c <= 14; c++)
 		{
-			num = count;
-			if (count > 9)
-			{
-				_putchar(1 + 48);
-				num = count % 10;
-			}
-			_putchar(num + 48);
+			if (c >= 10)
+				_putchar (c / 10 + '0');
+			_putchar (c % 10 + '0');
 		}
-		_putchar("\n");
+		_putchar ('\n');
 	}
 }
