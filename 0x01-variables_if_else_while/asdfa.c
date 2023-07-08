16f1301173384fa23005ad1d05c0ef013bd68460
mohamedtolan225 <mohamedtolan225@gmail.com>
2023-07-07 17:07:07 -0700

    asd

---
+++ 0x01-variables_if_else_while/asdfa.c
@@ -0,0 +1,18 @@
+#include <stdio.h>            
+/**                           
+ * main - prints the alphabet.
+ *                            
+ * Return: Always 0 (Success) 
+ */                           
+int main (void)               
+{                             
+char var = 'a';               
+while (var <= 'z')            
+{                             
+putchar(var);                 
+var++;                        
+}                             
+                              
+putchar('\n');                
+                              
+return (0);                                           
