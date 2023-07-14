0e8d2732606ddb7bf185b047830bf27fc33f87d8
mohamedtolan225 <mohamedtolan225@gmail.com>
2023-07-15 00:38:41 +0300

    asdas

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,7 +5,8 @@
 * @b: number of bytes to allocate
 *
 * Return: a pointer to the allocated memory
-*/void *malloc_checked(unsigned int b)
+*/
+void *malloc_checked(unsigned int b)
 {
 void *ptr;
 ptr = malloc(b);
