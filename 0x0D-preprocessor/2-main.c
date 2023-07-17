98f2b5fffdfb27f980a7f6be224f1ba9eacbaea3
mohamedtolan225 <mohamedtolan225@gmail.com>
2023-07-18 00:44:09 +0300

    asdsa

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - prints the name of the file
- *
- * Return: Always 0 (Success)
+ *main - prints the name of the file it was compiled from
+ *Return: 0
  */
-int ain(void)
+int main(void)
 {
-	printf("%s\n", __FILE__);
-	return (0);
+printf("%s\n", __FILE__);
+return (0);
 }
