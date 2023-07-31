b91c40f51c22e6b6285ac6ea8c60ca9fa2de4630
mohamedtolan225 <mohamedtolan225@gmail.com>
2023-07-31 19:46:31 +0300

    asdsa

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,20 +1,23 @@
 #include "lists.h"
 /**
-* get_nodeint_at_index - returns the node at a certain index in a linked
-list
-* @head: first node in the linked list
-* @index: index of the node to return
-*
-* Return: pointer to the node we're looking for, or NULL
-*/
+ * get_nodeint_at_index - return the nth node starting at 0
+ * @head: takes in struct listint_t *head
+ * @index: takes in an index
+ * Return: the integer walker points to
+ */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-unsigned int i = 0;
-listint_t *temp = head;
-while (temp && i < index)
+listint_t *move;
+unsigned int con = 0;
+move = head;
+while (move != NULL)
 {
-temp = temp->next;
-i++;
+if (con == index)
+{
+return (move);
 }
-return (temp ? temp : NULL);
+con++;
+move = move->next;	
+}
+return (move);
 }
